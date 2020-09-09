2abf1de1ac0e1e3af67ee2ea311743a294fa7c7a
Nathan Fain <cyphunk@gmail.com>
2020-09-09 16:42:41 +0200

    .

--- sync.sh
+++ sync.sh
@@ -4,11 +4,14 @@ EXCLUDEFLAGS="
 --exclude configuration.sh
 --exclude node_modules
 --exclude script_praha.txt
+--exclude script_applestore.txt
+--exclude script_current.txt
 --exclude .git
 "
 [ $# -le 0 ] && echo "$0 <ip>" && exit 1
 #IP=192.168.4.1
 IP=$1
 set -x
-rsync -avz $EXCLUDEFLAGS -e ssh ./ pi@${IP}:/home/pi/wwwserver/
+#rsync -avz $EXCLUDEFLAGS -e ssh ./ pi@${IP}:/home/pi/wwwserver/
+rsync -avz $EXCLUDEFLAGS -e ssh ./ pi@${IP}:/home/pi/surrogate-witch/
 set +x
